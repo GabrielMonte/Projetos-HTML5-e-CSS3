a40448de8c5e9dc8dfc6c8e7b51cee084021da6a
Gabriel Do Monte Vilhena <92929267+GabrielMonte@users.noreply.github.com>
2022-12-21 09:46:35 -0300

    tt

---
+++ projeto11/public/javascript/api.js
@@ -0,0 +1,6 @@
+let api_key = "4671ca43dd21ba96008d444b9d08da83"
+
+let img_url ="https://image.tmdb.org/t/p/w500/"
+let genres_list_http ="https://api.themoviedb.org/3/genre/movie/list?"
+
+let movia_genres_http = "https://api.themoviedb.org/3/certification/movie/list?"
